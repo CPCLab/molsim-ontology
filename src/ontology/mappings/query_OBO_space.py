2c3546ceb731b9a50e9418fa6c63802eaec88490
Hannah-Doerpholz <h.doerpholz@fz-juelich.de>
2025-03-05 16:48:01 +0100

    cleanup of the mapping code

--- src/ontology/mappings/query_OBO_space.py
+++ src/ontology/mappings/query_OBO_space.py
@@ -44,7 +44,7 @@ def makedirs(path):
         os.makedirs(path)
 
 # getting the namespace abbreviation based on the namespace url
-def namespacing(elem):
+def namespacing(elem, namespaces):
     # remove brackets from etree parsing
     sep = elem[1:].split("}")
     # getting the namespace abbreviation from the namespaces dict
@@ -56,14 +56,14 @@ def namespacing(elem):
     return sep
 
 # getting the attribute information of a parsed object
-def ns_att(elem):
+def ns_att(elem, namespaces):
     # ignore None elements
     if elem == {}:
         # returning None
         return ""
     # join the attribute dict together as a string
     for key in elem:
-        ns = namespacing(key) + '="' + elem[key] + '"'
+        ns = namespacing(key, namespaces) + '="' + elem[key] + '"'
         # print("RES", elem[key])
         # returning the joined string
         return ns
@@ -75,7 +75,7 @@ def html_characters(text):
 
 
 #################### parsing one owl object ##################
-def parse_element(element, i=0):
+def parse_element(element, namespaces, i=0):
     # declaring the output dict
     if len(element) == 0:
         result = None
@@ -85,10 +85,10 @@ def parse_element(element, i=0):
     for child in element:
         # getting the namespace annotation
         owl_object = ""
-        owl_object = owl_object + namespacing(child.tag)
+        owl_object = owl_object + namespacing(child.tag, namespaces)
 
         # getting the resource annotation
-        attributes = ns_att(child.attrib)
+        attributes = ns_att(child.attrib, namespaces)
 
         # creating the key from the namespace and resource annotation        
         if attributes != None:
@@ -103,7 +103,7 @@ def parse_element(element, i=0):
         
         # checking if leaf level is reached, if not, call the parser again
         if len(child) > 0:
-            result[tag] = parse_element(child, i)
+            result[tag] = parse_element(child, namespaces, i)
         # if leaf level is reached, save text if exists
         else:
             if child.text == None:
@@ -177,230 +177,256 @@ def replace_semicolon_list(element):
         return element  # Leave other data types unchanged
 ################# replace unicode hexcode with semicolon  #################
 
-
-################ reading the base input file ##############
-wd = os.getcwd()
-wd="/home/hannah/UsadellabARCs_small/molsim-ontology/"
-
-# if len(sys.argv) != 2:
-#     print("Usage: python3 prep_BD_files.py <config.yml>")
-#     sys.exit(1)
-
-# config_path = sys.argv[1]
-config_path = os.path.join(wd, "src/ontology/mappings/check_api.yml")
-
-if not os.path.exists(config_path):
-    print("Error: the yml file you put in doesn't exist or the file path is wrong.")
-    sys.exit(1)
-    
-with open(config_path, "r") as f:
-    config = yaml.safe_load(f)
-    
-input_file = os.path.join(wd, config["input"]["filled_ontology_file"])
-obo_ontologies_file = os.path.join(wd, config["input"]["obo_ontologies_file"])
-output_file = os.path.join(wd, config["output"]["term_suggestion_list"])
-
-
-makedirs(os.path.dirname(output_file))
-# opening a base ontology structure to be parsed
-with open(input_file, "r") as file:
-    owl_content = file.read()
-################ reading the base input file ##############
-
-################ parsing the base header ################
-parsing_start = time.time()
-# pre-defining the header as rdf
-onto_head = {"rdf:RDF":{}}
-header = re.search(r'<rdf:RDF([^>]*)>', owl_content)
-namespaces = {}
-# searching for the RDF header to parse
-if header:
-    # finding all attributes of the header
-    rdf_rdf_attributes = header.group(1)
-    attributes = re.findall(r'([\w:]+)=["\'](.*?)["\']', rdf_rdf_attributes)
-    # saving the header attributes in a dict
-    for key, value in attributes:
-        onto_head["rdf:RDF"][key] = value
-        if len(key.split(":")) == 1:
-            pass
-        else: 
-            namespaces[value] = key.split(":")[1]
-else:
-    print("rdf:RDF element not found in the file.")
-################ parsing the base header ################
-
-
-
-################ parsing the content ################
-tree = ET.parse(input_file)
-root = tree.getroot()
-terms = {}
-ontology = {}
-annotation_properties = {}
-classes = {}
-object_properties = {}
-data_properties = {}
-axioms = {}
-instances = {}
-tans = {}
-sources = {}
-
-
-# iterating over all ontology elements
-for child in root[:]:
-    # getting the namespace annotation
-    owl_object = ""
-    owl_object = owl_object + (namespacing(child.tag))
-
-    # getting the resource annotation
-    attribs = ns_att(child.attrib)
-    res = owl_object + " " + attribs
-    
-    # parsing the object
-    parsed_element = parse_element(child)
-    
-    # saving the parsed object
-    if "Ontology" in owl_object:
-        ontology[res] = parsed_element
-    elif "AnnotationProperty" in owl_object:
-        annotation_properties[res] = parsed_element
-    elif "ObjectProperty" in owl_object:
-        object_properties[res] = parsed_element
-    elif "Class" in owl_object:
-        classes[res] = parsed_element
-    elif "DatatypeProperty" in owl_object:
-        data_properties[res] = parsed_element
-    elif "NamedIndividual" in owl_object:
-        instances[res] = parsed_element
-    elif "Axiom" in owl_object:
-        axioms[res] = parsed_element
-    terms[res] = parsed_element
-parsing_end = time.time()
-################ parsing the content ################
-
-
-labels = find_lables(classes)
-labels = replace_semicolon(labels)
-
-with open(obo_ontologies_file, "r") as f:
-    ontologies_json = json.load(f)
-ontologies = [entry["id"] for entry in ontologies_json["ontologies"]]
-ontologies = ""
-for entry in ontologies_json["ontologies"]:
-    ontologies = ontologies + entry["id"] + "," 
-if ontologies[-1] == ",":
-    ontologies = ontologies[:-1]
-
-
-# TS4TIB API
-# api_url = "https://service.tib.eu/ts4tib/api/search"
-# options = OrderedDict()
-# options["schema"] = "collection"
-# options["classification"] = "DataPLANT"
-# options["exclusiveFilter"] = "false"
-# options["obsoletes"] = "false"
-# options["local"] = "false"
-
-# OLS4 API
-api_url = "https://www.ebi.ac.uk/ols4/api/search"
-options = OrderedDict()
-options["ontology"] = ontologies
-options["queryFields"] = "label"
-options["obsoletes"] = "false"
-options["local"] = "true"
-options["exact"] = "false"
-
-suggestions = {}
-
-
-def preprocess_label(rdfslabel):
+################# preprocess concept labels  #################
+def preprocess_label(rdfslabel, lemmatizer):
     return {lemmatizer.lemmatize(word.lower()) for word in word_tokenize(rdfslabel) }
+################# preprocess concept labels  #################
 
-
-def jaccard(term1, term2):
-    set1 = preprocess_label(term1)
-    set2 = preprocess_label(term2)
+################# calculate jaccard index #################
+def jaccard(term1, term2, lemmatizer):
+    set1 = preprocess_label(term1, lemmatizer)
+    set2 = preprocess_label(term2, lemmatizer)
     return len(set1 & set2) / len(set1 | set2)
+################# calculate jaccard index #################
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
-lemmatizer = WordNetLemmatizer()
+################# opening the ontology file #################
+# opening a base ontology structure to be parsed
+def read_rdfxml(file):
+    with open(file, "r") as file:
+        owl_content = file.read()
+    return owl_content
+################# opening the ontology file #################
 
+################ parsing the base header ################
+def parse_ontology_header(owl_content):
+    # pre-defining the header as rdf
+    onto_head = {"rdf:RDF":{}}
+    header = re.search(r'<rdf:RDF([^>]*)>', owl_content)
+    namespaces = {}
+    # searching for the RDF header to parse
+    if header:
+        # finding all attributes of the header
+        rdf_rdf_attributes = header.group(1)
+        attributes = re.findall(r'([\w:]+)=["\'](.*?)["\']', rdf_rdf_attributes)
+        # saving the header attributes in a dict
+        for key, value in attributes:
+            onto_head["rdf:RDF"][key] = value
+            if len(key.split(":")) == 1:
+                pass
+            else: 
+                namespaces[value] = key.split(":")[1]
+    else:
+        print("rdf:RDF element not found in the file.")
+    
+    return namespaces
+################ parsing the base header ################
 
-i = 0
-for query in labels[:]:
-    print(i)
-    if "MOLSIM" in query[1] and "DEPRECATED" not in query[1]:
-        options["q"] = query[0]
+################ parsing the content ################
+def parse_ontology(file, namespaces):
+    tree = ET.parse(file)
+    root = tree.getroot()
+    terms = {}
+    ontology = {}
+    annotation_properties = {}
+    classes = {}
+    object_properties = {}
+    data_properties = {}
+    axioms = {}
+    instances = {}
+    tans = {}
+    sources = {}
     
     
-        test_url = api_url + "?"
-        for key, value in options.items():
-            test_url = test_url + key + "=" + value + "&"
+    # iterating over all ontology elements
+    for child in root[:]:
+        # getting the namespace annotation
+        owl_object = ""
+        owl_object = owl_object + (namespacing(child.tag, namespaces))
+    
+        # getting the resource annotation
+        attribs = ns_att(child.attrib, namespaces)
+        res = owl_object + " " + attribs
         
-        response = requests.get(test_url)
-        results = json.loads(response.content)["response"]["docs"]
-        for elem in results[:10]:
-            iri = elem["iri"]
-            obo_id = elem["obo_id"]
-            label = elem["label"]
-            if "ontology_prefix" in elem:
-                on = elem["ontology_prefix"]
-            elif "ontology_name" in elem:
-                on = elem["ontology_name"]
-            else:
-                print("no ontology prefix")
-                on = ""
-            key = query[1] + "###" + obo_id
+        # parsing the object
+        parsed_element = parse_element(child, namespaces)
+        
+        # saving the parsed object
+        if "Ontology" in owl_object:
+            ontology[res] = parsed_element
+        elif "AnnotationProperty" in owl_object:
+            annotation_properties[res] = parsed_element
+        elif "ObjectProperty" in owl_object:
+            object_properties[res] = parsed_element
+        elif "Class" in owl_object:
+            classes[res] = parsed_element
+        elif "DatatypeProperty" in owl_object:
+            data_properties[res] = parsed_element
+        elif "NamedIndividual" in owl_object:
+            instances[res] = parsed_element
+        elif "Axiom" in owl_object:
+            axioms[res] = parsed_element
+        terms[res] = parsed_element
+        
+    return terms, ontology, annotation_properties, classes, object_properties, data_properties, axioms, instances, tans, sources
+################ parsing the content ################
+
+################ retrieving all ontologies in obo foundry from their json ################
+# uses the public json file from here: http://obofoundry.org/registry/ontologies.jsonld
+def get_obo_foundry_ontologies(obo_ontologies_file):
+    with open(obo_ontologies_file, "r") as f:
+        ontologies_json = json.load(f)
+    ontologies = [entry["id"] for entry in ontologies_json["ontologies"]]
+    ontologies = ""
+    for entry in ontologies_json["ontologies"]:
+        ontologies = ontologies + entry["id"] + "," 
+    if ontologies[-1] == ",":
+        ontologies = ontologies[:-1]
+    
+    ontologies_df = pd.DataFrame.from_dict(ontologies_json["ontologies"])
+    ontologies_df.index = ontologies_df["id"]
+    ontologies_df = ontologies_df[["activity_status"]]
+    
+    ontologies_df.to_csv(ontology_status, sep="\t")
+    
+    return ontologies
+################ retrieving all ontologies in obo foundry from their json ################
+
+################ mapping terms against ols4 ################
+def ols4_mapping(ontologies, idspace, number_of_inputs=None, number_of_hits=10):
+    # OLS4 API
+    api_url = "https://www.ebi.ac.uk/ols4/api/search"
+    options = OrderedDict()
+    options["ontology"] = ontologies
+    options["queryFields"] = "label"
+    options["obsoletes"] = "false"
+    options["local"] = "true"
+    options["exact"] = "false"
+    
+    suggestions = {}
+    
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    input_lemmatizer = WordNetLemmatizer()
+    
+    i = 0
+    for query in input_labels[:number_of_inputs]:
+        print(i)
+        if idspace in query[1] and "DEPRECATED" not in query[1]:
+            options["q"] = query[0]
+        
+        
+            test_url = api_url + "?"
+            for key, value in options.items():
+                test_url = test_url + key + "=" + value + "&"
             
-            if ":" not in obo_id:
-                obo_id = on + ":" + obo_id
-            if "description" in elem:
-                if len(elem["description"]) > 0:
-                    description = " ".join(elem["description"])
+            response = requests.get(test_url)
+            results = json.loads(response.content)["response"]["docs"]
+            for elem in results[:number_of_hits]:
+                # iri = elem["iri"]
+                obo_id = elem["obo_id"]
+                label = elem["label"]
+                if "ontology_prefix" in elem:
+                    on = elem["ontology_prefix"]
+                elif "ontology_name" in elem:
+                    on = elem["ontology_name"]
+                else:
+                    print("no ontology prefix")
+                    on = ""
+                key = query[1] + "###" + obo_id
+                
+                if ":" not in obo_id:
+                    obo_id = on + ":" + obo_id
+                if "description" in elem:
+                    if len(elem["description"]) > 0:
+                        description = " ".join(elem["description"])
+                    else:
+                        description = ""
                 else:
                     description = ""
-            else:
-                description = ""
-            if key not in suggestions:
-                vector1 = model.encode(query[2]).reshape(1, -1)
-                vector2 = model.encode(description).reshape(1, -1)
-                definition_score = cosine_similarity(vector1, vector2)[0][0]
-                
-                label_score = jaccard(query[0], label)
-                score = (0.7*label_score) + (0.3*definition_score)
-                
-                if score > 0.7:
-                    suggestions[key]= (query[0], query[1].split("org/obo/")[1].replace("_", ":").lower(), label, obo_id, on, score)
-                # print(query[0], "\t", label, "\t", label_score, "\t", definition_score, "\t", score)
-
-            
-    i += 1
-
-
-data = [value for value in suggestions.values()]
-df = pd.DataFrame(data, columns=["subject_label", "subject_id", "object_label", "object_id", "object_source", "confidence"])
-
-ns_dict = {}
-
-api_url = "https://www.ebi.ac.uk/ols4/api/ontologies/"
-
-for query in df["object_source"].unique():
-    test_url = api_url + query
-    response = requests.get(test_url)
-    results = json.loads(response.content)
+                if key not in suggestions:
+                    vector1 = model.encode(query[2]).reshape(1, -1)
+                    vector2 = model.encode(description).reshape(1, -1)
+                    definition_score = cosine_similarity(vector1, vector2)[0][0]
+                    
+                    label_score = jaccard(query[0], label, input_lemmatizer)
+                    score = (0.7*label_score) + (0.3*definition_score)
+                    
+                    if score > 0.7:
+                        suggestions[key]= (query[0], query[1].split("org/obo/")[1].replace("_", ":").lower(), label, obo_id, on, score)
+                    # print(query[0], "\t", label, "\t", label_score, "\t", definition_score, "\t", score)
     
-    version_iri = results["config"]["versionIri"]
-    file_location = results["config"]["fileLocation"]
-    ns_dict[query] = (file_location, version_iri)
+        i += 1
+    
+    
+    data = [value for value in suggestions.values()]
+    df = pd.DataFrame(data, columns=["subject_label", "subject_id", "object_label", "object_id", "object_source", "confidence"])
+    
+    return df
+################ mapping terms against ols4 ################
+
+################ create sssom mapping file ################
+def create_sssom_mapping_file(df, save_output=False):
+    ns_dict = {}
+    
+    api_url = "https://www.ebi.ac.uk/ols4/api/ontologies/"
+    
+    for query in df["object_source"].unique():
+        print(query)
+        test_url = api_url + query
+        print(test_url)
+        response = requests.get(test_url)
+        results = json.loads(response.content)
+        
+        version_iri = results["config"]["versionIri"]
+        file_location = results["config"]["fileLocation"]
+        ns_dict[query] = (file_location, version_iri)
+
+    
+    return ns_dict
+################ create sssom mapping file ################
 
 
-df["object_source"] = df["object_source"].map(lambda x: ns_dict[x][0])
-df["object_source_version"] = df["object_source"].map(lambda x: next((value[1] for key, value in ns_dict.items() if value[0] == x), None))
-df["predicate_id"] = "skos:closeMatch"
-df["mapping_justification"] = "semapv:CompositeMatching"
-df["mapping_date"] = date.today()
-df["mapping_tool"] = "https://github.com/CPCLab/molsim-ontology/tree/main/src/ontology/mappings/query_OBO_space.py"
-df["creator_id"] = "orcid:0000-0002-0476-9699"
 
-df.to_csv(output_file, sep="\t", index=False)
+################ reading the base input file ##############
+if __name__ == "__main__":
+    wd = os.getcwd()
+    wd="/home/hannah/UsadellabARCs_small/molsim-ontology/"
+    
+    # if len(sys.argv) != 2:
+    #     print("Usage: python3 prep_BD_files.py <config.yml>")
+    #     sys.exit(1)
+    
+    # config_path = sys.argv[1]
+    config_path = os.path.join(wd, "src/ontology/mappings/check_api.yml")
+    
+    if not os.path.exists(config_path):
+        print("Error: the yml file you put in doesn't exist or the file path is wrong.")
+        sys.exit(1)
+        
+    with open(config_path, "r") as f:
+        config = yaml.safe_load(f)
+        
+    input_file = os.path.join(wd, config["input"]["filled_ontology_file"])
+    input_obo_ontologies_file = os.path.join(wd, config["input"]["obo_ontologies_file"])
+    class_suggestion_list = os.path.join(wd, config["output"]["class_suggestion_list"])
+    ontology_status = os.path.join(wd, config["output"]["ontology_status"])
+    
+    makedirs(os.path.dirname(class_suggestion_list))
+    makedirs(os.path.dirname(ontology_status))
+    
+    input_content = read_rdfxml(input_file)
+    input_namespaces = parse_ontology_header(input_content)
+    input_terms, input_ontology, input_annotation_properties, input_classes, input_object_properties, input_data_properties, input_axioms, input_instances, input_tans, input_sources = parse_ontology(input_file, input_namespaces)
+    input_labels = find_lables(input_classes)
+    input_labels = replace_semicolon(input_labels)
+    input_ontologies = get_obo_foundry_ontologies(input_obo_ontologies_file)
+    input_df = ols4_mapping(input_ontologies, idspace="MOLSIM", number_of_inputs=3)
+    input_df = create_sssom_mapping_file(input_df, save_output=False)
+
+    
+    
+    
+
+
+
+
 
